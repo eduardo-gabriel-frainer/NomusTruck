8d2540b05129b7c32125eca2a756e3fd27e51453
EDUARDO GABRIEL FRAINER <eduardo_frainer@estudante.sesisenai.org.br>
2026-03-27 19:43:38 -0300

    first commit

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,8 @@
+export default function CardMenu({name, price}){
+    return(
+        <div className="bg-amber-800 w-100">
+            Nome: {name}
+            Preço: {price}
+        </div>
+    )
+}
